ba9bd2918fb8c6e58006f2c96932c7f4237f004f
Anuoluwapo <adestudy52@gmail.com>
2026-06-25 16:18:47 +0100

    FAQ & FOOTER

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import BotpressHero from '../components/ui/BotpressHero';
 import FeaturesSection from '../components/ui/FeaturesSection';
 import ManagementSection from '../components/ui/ManagementSection';
 import PricingSection from '../components/ui/PricingSection';
+import FAQAndFooter from '../components/ui/FAQAndFooter';
 import './LandingPage.css';
 
 export default function LandingPage() {
@@ -15,6 +16,7 @@ export default function LandingPage() {
         <FeaturesSection />
         <ManagementSection />
         <PricingSection />
+        <FAQAndFooter />
       </main>
     </div>
   );
